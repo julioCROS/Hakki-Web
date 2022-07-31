400c21faf13acd148eeb6b5a498e6740c239be53
Julio Cesar Rocha <julio.1009@hotmail.com>
2022-07-31 17:18:25 -0300

    [+] Project start + Index page create

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+import '../styles/ResponsiveModal.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
