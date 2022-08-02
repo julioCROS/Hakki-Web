ab2b797a8e35cb0b3cca74f27f9d57ef1fcbff31
Julio Cesar Rocha <julio.1009@hotmail.com>
2022-08-02 14:22:20 -0300

    [+] Created header with main infos in 'slug docente' page

--- components/Title/index.js
+++ components/Title/index.js
@@ -2,9 +2,11 @@ import styles from './Styles.module.css';
 
 export default function Title(){
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Hakki</h1>
-      <h1 className={styles.subtitle}>학기</h1> 
-    </div>
+    <a href="https:\\hakki.vercel.app/">
+      <div className={styles.container}>
+        <h1 className={styles.title}>Hakki</h1>
+        <h1 className={styles.subtitle}>학기</h1> 
+      </div>
+    </a>
   )
 }
--- pages/docente/[Slug].js
+++ pages/docente/[Slug].js
@@ -1,11 +1,33 @@
 import axios from 'axios';
 import { useState } from 'react';
 import styles from '../../styles/Docente.module.css';
+import { makeStyles } from "@material-ui/core/styles";
 
 import Title from '../../components/Title';
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
 
 const apiUrl_professores = 'https://hakki-api.herokuapp.com/professores';
 const apiUrl_reviews = 'https://hakki-api.herokuapp.com/reviews';
+
+const useStyles = makeStyles(theme => ({
+  arrow: {
+    "&:before": {
+      border: "2px solid #c9c9c9"
+    },
+    color: "#c9c9c9",
+  },
+  tooltip: {
+    backgroundColor: "#c9c9c9",
+    color: "black",
+    fontSize: "15px",
+    margin: "10px",
+    padding: "10px",
+    paddingLeft: "25px",
+    paddingRight: "25px",
+    fontFamily: "Ubuntu",
+    fontWeight: "bold",
+  }
+}));
 
 Docente.getInitialProps = async (context) => {
   const { req, query, res, asPath, pathname } = context;
@@ -19,12 +41,14 @@ Docente.getInitialProps = async (context) => {
 
 export default function Docente({ data }) { 
   const [professor, setProfessor] = useState(data);
+  const toolTipClasses = useStyles();  
 
   if(data.length === 0) {
     return (
       <div className={styles.main}>
         <Title />
         <p className={styles.noDataFound}>Professor não encontrado.</p>
+        <a href="../"><p className={styles.backString}>Voltar ao inicio.</p></a>
       </div>
     );
   }
@@ -32,7 +56,24 @@ export default function Docente({ data }) {
     return(
       <div className={styles.main}>
         <Title />
-        <p className={styles.noDataFound}>{professor.nome}</p>
+        <div className={styles.container}>
+          <div className={styles.content}>
+            <div className={styles.mainInfo}>
+              {professor.nome}
+              <div className={styles.secondInfo}>
+                <div className={styles.reviewsInfo}><b>Avaliações:</b> {professor.reviews.length}</div>
+                <div className={styles.cadastrar}>Cadastrar avaliação</div>
+              </div>
+            </div>
+            <div className={styles.averages}>
+              <div className={styles.values}>
+              <Tooltip classes={{ arrow: toolTipClasses.arrow, tooltip: toolTipClasses.tooltip }} disableFocusListener disableTouchListener title="Qualidade" placement="top" arrow>{professor.mediaQualidade == null ? <p>X</p> : professor.mediaQualidade >= 7 ? <p className={styles.goodAvg}>{professor.mediaQualidade}</p> :professor.mediaQualidade >= 5 ? <p className={styles.neutralAvg}>{professor.mediaQualidade}</p> : <p className={styles.chaoticAvg}>{professor.mediaQualidade}</p>}</Tooltip >
+              <Tooltip classes={{ arrow: toolTipClasses.arrow, tooltip: toolTipClasses.tooltip }} disableFocusListener disableTouchListener title="Facilidade" placement="top" arrow>{professor.mediaFacilitacao == null ? <p>X</p> : professor.mediaFacilitacao >= 7 ? <p className={styles.goodAvg}>{professor.mediaFacilitacao}</p> :professor.mediaFacilitacao >= 5 ? <p className={styles.neutralAvg}>{professor.mediaFacilitacao}</p> : <p className={styles.chaoticAvg}>{professor.mediaFacilitacao}</p>}</Tooltip >
+              <Tooltip classes={{ arrow: toolTipClasses.arrow, tooltip: toolTipClasses.tooltip }} disableFocusListener disableTouchListener title="Média" placement="top" arrow>{professor.mediaQualidade == null  ? <p>X</p> : ((professor.mediaQualidade + professor.mediaFacilitacao) / 2) >= 7 ? <p className={styles.goodAvg}>{((professor.mediaQualidade + professor.mediaFacilitacao) / 2)}</p> :((professor.mediaQualidade + professor.mediaFacilitacao) / 2) >= 5 ? <p className={styles.neutralAvg}>{((professor.mediaQualidade + professor.mediaFacilitacao) / 2)}</p> : <p className={styles.chaoticAvg}>{((professor.mediaQualidade + professor.mediaFacilitacao) / 2)}</p>}</Tooltip >
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
