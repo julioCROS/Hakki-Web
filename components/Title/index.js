ef29d4ca744821bf776714fb8b3be60b4a13bb76
Julio Cesar Rocha <julio.1009@hotmail.com>
2022-08-02 15:04:42 -0300

    Slug page style update + Title link fix

--- components/Title/index.js
+++ components/Title/index.js
@@ -2,11 +2,9 @@ import styles from './Styles.module.css';
 
 export default function Title(){
   return (
-    <a href="https:\\hakki.vercel.app/">
       <div className={styles.container}>
-        <h1 className={styles.title}>Hakki</h1>
-        <h1 className={styles.subtitle}>학기</h1> 
+        <a href="https:\\hakki.vercel.app/"><h1 className={styles.title}>Hakki</h1></a>
+        <a href="https:\\hakki.vercel.app/"><h1 className={styles.subtitle}>학기</h1></a>
       </div>
-    </a>
   )
 }
