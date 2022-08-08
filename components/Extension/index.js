49cddaf30758d0a34c8c1278005d5a9872899702
Julio Cesar Rocha <julio.1009@hotmail.com>
2022-08-08 13:08:03 -0300

    [+] Add extension link

--- components/Extension/index.js
+++ components/Extension/index.js
@@ -5,7 +5,7 @@ export default function ExtensionContainer(){
   return(
     <div className={styles.container}>
       <div className={styles.extension}>
-        <Image src="/extension.png" width={330} height={100} layout="responsive" className="card-img-top" alt="..."/>
+        <a href="https://chrome.google.com/webstore/detail/hakki/cfeknhlenfklocphhgfgnjagagdfpdkc?hl=pt-br" target="_blank"><Image src="/extension.png" width={330} height={100} layout="responsive" className="card-img-top" alt="..."/></a>
       </div>
     </div>
   )
